6b79afeffd9cd9851c904c5d3eb236662fba5ef9
Yehor Vikulov <20969918+yvikulov@users.noreply.github.com>
2022-08-19 21:12:31 +0300

    clean up output

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,6 @@ int main(void)
 {
     const char * path = "./assets/wireframe.txt";
     Wireframe wireframe(path);
-
-    for (size_t i = 0; i < wireframe.getRow(); i++)
-    {
-        for (size_t j = 0; j < wireframe.getColumn(); j++)
-        {
-            std::cout << wireframe[i][j] << " ";
-        }
-        std::cout << std::endl;
-    }
     
     return Engine(wireframe).run();
 }
